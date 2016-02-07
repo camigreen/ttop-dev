20a02f0a907056d82af8c15dfc57a0777204f9c3
Shawn Gibbons <sgibbons@palmettoimages.com>
2016-02-07 14:21:57 -0500

    updates

--- media/template/yoo_eat.php
+++ media/template/yoo_eat.php
@@ -1,6 +1,6 @@
 {
-    "tmpl_check": "9e12fbdf0a121306ef644cd69fdcffa7",
+    "tmpl_check": "1185902fd404f335daeb2e8c1fe63bcf",
     "tmpl_data": "{\"status\":\"up-to-date\",\"message\":\"You're up to date!. 1.0.8 is currently the newest version available.\"}",
-    "warp_check": "589056a459d70ea44ff342b406ff4fdc",
+    "warp_check": "40d2f5d0e35a06109900f8f97044e0ea",
     "warp_data": "{\"status\":\"error\",\"message\":\"Sorry dude, this application is unknown.\"}"
 }
