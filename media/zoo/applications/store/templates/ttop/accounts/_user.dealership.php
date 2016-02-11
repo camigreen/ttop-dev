8b02755f968192d90ec62ad09c587c90fc97cecc
Shawn Gibbons <sgibbons@palmettoimages.com>
2016-02-11 12:26:32 -0500

    fixed issues with user groups on user account edit/add page

--- media/zoo/applications/store/templates/ttop/accounts/_user.dealership.php
+++ media/zoo/applications/store/templates/ttop/accounts/_user.dealership.php
@@ -45,6 +45,7 @@
 			<?php 
 				$values['parents'] = $this->account->getParents();
 				$values['groups'] = $this->account->getUser()->getAuthorisedGroups();
+				unset($values['groups'][0]);
 				$this->form->setValues($values);
 			?>
 			<?php if($this->form->checkGroup('related')) : ?>
