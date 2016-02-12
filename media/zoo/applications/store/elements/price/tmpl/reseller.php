c00110b11727586d09d7f6eba816c36c02005842
Shawn Gibbons <sgibbons@palmettoimages.com>
2016-02-12 13:15:39 -0500

    Fixed UBSK Markup Modal

--- media/zoo/applications/store/elements/price/tmpl/reseller.php
+++ media/zoo/applications/store/elements/price/tmpl/reseller.php
@@ -6,16 +6,15 @@ $price = $item->getPrice();
 <div id="<?php echo $item->id; ?>-price">
 	<i class="currency"></i>
 	<span class="price"><?php echo $this->app->number->precision($price->get('markup'), 2); ?></span>
-	<a id="price_display-<?php echo $item->id; ?>" href="#"class="uk-icon-button uk-icon-info-circle uk-text-top" style="margin-left:10px;" data-uk-tooltip data-id="<?php echo $item->id; ?>" title="Click here for pricing info!"></a>
+	<a id="price_display-<?php echo $item->id; ?>" href="#"class="uk-icon-button uk-icon-info-circle uk-text-top markup-modal" style="margin-left:10px;" data-uk-tooltip data-id="<?php echo $item->id; ?>" title="Click here for pricing info!"></a>
 	<input type="hidden" name="markup" data-name="Markup" value="<?php echo $price->getMarkupRate(); ?>" />
 </div>
 
 <script>
 	jQuery(function($){
 		$(document).ready(function(){
-			$('#price_display-<?php echo $item->id; ?>').on('click', function(e) {
-				var modal;
-				var id = $(this).parents('.price-main').data('id') ? $(this).parents('.price-main').data('id') : $(this).data('id');
+			$('.markup-modal').off('click').on('click', function(e) {
+				var id = $(e.target).parents('.price-main').data('id') ? $(e.target).parents('.price-main').data('id') : $(e.target).data('id');
 				console.log(id);
 				var StoreItem = $("#storeOrderForm").StoreItem('getItem',id);
 				console.log(StoreItem);
