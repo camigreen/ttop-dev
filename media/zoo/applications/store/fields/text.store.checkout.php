62294e50dccdf925c39ecd4218124e166d48cd7e
camigreen <sgibbons@palmettoimages.com>
2016-02-17 14:42:09 -0500

    updates

---
+++ media/zoo/applications/store/fields/text.store.checkout.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package   com_zoo
+ * @author    YOOtheme http://www.yootheme.com
+ * @copyright Copyright (C) YOOtheme GmbH
+ * @license   http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+
+// set attributes
+$control_name = $control_name == 'null' ? $name : $control_name."[$name]";
+$attributes = array('id' => $name,'type' => 'text', 'name' => "$control_name", 'value' => $value, 'class' => isset($class) ? $class : '');
+
+printf('<input %s />', $this->app->field->attributes($attributes, array('label', 'description', 'default')));
