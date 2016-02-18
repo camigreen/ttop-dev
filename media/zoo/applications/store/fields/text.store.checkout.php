890030dda20d527452f6289fe33b44c60cbfe475
Shawn Gibbons <sgibbons@palmettoimages.com>
2016-02-18 09:11:15 -0500

    updates

--- media/zoo/applications/store/fields/text.store.checkout.php
+++ media/zoo/applications/store/fields/text.store.checkout.php
@@ -10,4 +10,9 @@
 $control_name = $control_name == 'null' ? $name : $control_name."[$name]";
 $attributes = array('id' => $name,'type' => 'text', 'name' => "$control_name", 'value' => $value, 'class' => isset($class) ? $class : '');
 
+if($node->attributes()->disabled) {
+	$attributes['disabled'] = true;
+} 
+
+
 printf('<input %s />', $this->app->field->attributes($attributes, array('label', 'description', 'default')));
