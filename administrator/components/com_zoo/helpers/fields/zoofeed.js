9e2e3862274bd1bc70613454f0a01f5376769107
camigreen <sgibbons@palmettoimages.com>
2016-07-25 13:23:47 -0400

    removed items from cache

--- administrator/components/com_zoo/helpers/fields/zoofeed.js
+++
@@ -1,3 +0,0 @@
-/* Copyright (C) YOOtheme GmbH, http://www.gnu.org/licenses/gpl.html GNU/GPL */
-
-jQuery(function(i){i("div.zoo-feed").each(function(){var n=i(this).find("div.input"),d=i(this).find("input:radio");d.first().is(":checked")&&n.hide(),d.bind("change",function(){n.slideToggle()})})});
